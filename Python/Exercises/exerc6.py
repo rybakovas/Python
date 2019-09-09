fbeddb5165f42282fff868cf86a205e753a35f5c
Victor Rybakovas <rybakovas@gmail.com>
2019-09-09 17:55:40 -0300

    Update Commit
    Signed-off-by: Victor Rybakovas <rybakovas@gmail.com>

---
+++ Python/Exercises/exerc6.py
@@ -0,0 +1,22 @@
+#Leap day def
+
+from math import *
+
+
+def is_leap(year):
+    leap = False
+
+    if year >= 1900 and year <= (pow(10, 5)):
+        if year % 4 == 0:
+            if year % 400 == 0:
+                leap = True
+            elif year % 100 == 0:
+                leap = False
+
+    return leap
+
+
+year = int(input("Type a Year:"))
+print("Is the year lead? {}".format(is_leap(year)))
+
+# By Victor Rybakovas Sep 2019 - http://bit.ly/linkedin-victor
