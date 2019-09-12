e8e76af9b081547d39e3bf410a4278efc0ab1744
Victor Rybakovas <rybakovas@gmail.com>
2019-09-12 11:25:20 -0300

    update commit
    Signed-off-by: rybakovas <rybakovas@gmail.com>

--- Python/basic/Mobile_inheritance.py
+++ Python/basic/Mobile_inheritance.py
@@ -1,34 +1,36 @@
-class Mobile:
+class AutoMobile:
     def __init__(self, wheels=0):
         self.wheels = wheels
 
     def __str__(self):
-
         return str(self.wheels)
 
-    def GasCalc(self):
-
+    def gas_calc(self):
         return 10/2
 
-class Car(Mobile):
+
+class Car(AutoMobile):
     def __init__(self):
         super().__init__(wheels=4)
-    def __str__(self):
 
+    def __str__(self):
         return "The Car has " + str(self.wheels) + "!"
 
-class Moto(Mobile):
+
+class Moto(AutoMobile):
     def __init__(self):
         super().__init__(wheels=2)
 
-    def GasCalc(self):
+    def gas_calc(self):
         return super().GasCalc()/2
 
+# mobile = Mobile()
 
-#mobile = Mobile()
+
 mobile = Car()
 mobile2 = Moto()
 
-print(mobile.GasCalc())
-print(mobile2.GasCalc())
+print(mobile.gas_calc())
+print(mobile2.gas_calc())
+
 # By Victor Rybakovas Sep 2019 - http://bit.ly/linkedin-victor
